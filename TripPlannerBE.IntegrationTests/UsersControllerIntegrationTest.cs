6e40b66cbfc6688c33904aa0a8248ef639f61960
Vladyslav Shyrinov <vladyslav.shyrinov@verivox.com>
2019-09-19 16:40:51 +0200

    integration tests preparation

--- TripPlannerBE.IntegrationTests/UsersControllerIntegrationTest.cs
+++ TripPlannerBE.IntegrationTests/UsersControllerIntegrationTest.cs
@@ -1,39 +1,38 @@
+﻿using Entities.Models;
+using Newtonsoft.Json;
 using System;
-using System.Net;
+using System.Collections.Generic;
 using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace TripPlannerBE.IntegrationTests
 {
-    public class UsersControllerIntegrationTest : IClassFixture<TestFixture>
+    public class UsersControllerIntegrationTest : IClassFixture<CustomWebApplicationFactory<Startup>>
     {
         private readonly HttpClient _client;
 
-
-        public UsersControllerIntegrationTest(TestFixture fixture)
+        public UsersControllerIntegrationTest(CustomWebApplicationFactory<Startup> factory)
         {
-            _client = fixture.Client;
+            _client = factory.CreateClient();
         }
 
         [Fact]
-        public async void Test1()
+        public async Task CanGetUsers()
         {
-            // Arrange
-            var request = new HttpRequestMessage(
-                HttpMethod.Get, "/users");
+            // The endpoint or route of the controller action.
+            var httpResponse = await _client.GetAsync("/api/users");
 
-            // Act: request the /users route
-            var response = await _client.SendAsync(request);
+            // Must be successful.
+            httpResponse.EnsureSuccessStatusCode();
 
-            // Assert: the user is sent to the login page
-            Assert.Equal(
-                HttpStatusCode.OK,
-                response.StatusCode);
-
-            //Assert.Equal(
-            //    "http://localhost:8888/Account" +
-            //    "/Login?ReturnUrl=%2Ftodo",
-            //    response.Headers.Location.ToString());
+            // Deserialize and examine results.
+            var stringResponse = await httpResponse.Content.ReadAsStringAsync();
+            var users = JsonConvert.DeserializeObject<IEnumerable<User>>(stringResponse);
+            Assert.Empty(users);
+            //Assert.Contains(players, p => p.FirstName == "Wayne");
+            //Assert.Contains(players, p => p.FirstName == "Mario");
         }
     }
 }
